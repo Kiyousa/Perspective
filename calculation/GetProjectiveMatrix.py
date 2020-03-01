5d4b2564f04d74b110240167dc3e9bf3c39749e7
Kiyou <30902079+Kiyousa@users.noreply.github.com>
2020-03-02 01:17:33 +0800

    Update GetProjectiveMatrix.py

--- calculation/GetProjectiveMatrix.py
+++ calculation/GetProjectiveMatrix.py
@@ -3,13 +3,14 @@ import numpy as np
 import sympy
 
 # region points used for determining the projective matrix
-A = np.array([0, 0, 1])  # 0 0 1
+# all the points are writen in homogeneous coordinates
+A = np.array([0, 0, 1])
 a = np.array([0, -1, 1])
-B = np.array([-1, 1, 1])  # -1 1 1
+B = np.array([-1, 1, 1])
 b = np.array([-1 / 2, -1 / 2, 1])
-C = np.array([1, 1, 1])  # 1 1 1
+C = np.array([1, 1, 1])
 c = np.array([1 / 2, -1 / 2, 1])
-D = np.array([0, 2, 1])  # 0 3 1
+D = np.array([0, 2, 1])
 d = np.array([0, -1 / 3, 1])
 # endregion
 
@@ -22,13 +23,13 @@ cof = m0, m1, m2, m3, m4, m5, m6, m7
 expr_x = u * m0 + v * m1 - x * u * m6 - x * v * m7 - x + m2
 expr_y = u * m3 + v * m4 - y * u * m6 - y * v * m7 - y + m5
 
-eqs = []
+eqs = []  # form linera equations system
 
 for p in [(A, a), (B, b), (C, c), (D, d)]:
     eqs.append(expr_x.subs({u: p[0][0], v: p[0][1], x: p[1][0], y: p[1][1]}))
     eqs.append(expr_y.subs({u: p[0][0], v: p[0][1], x: p[1][0], y: p[1][1]}))
 
-A_, B_ = sympy.linear_eq_to_matrix(eqs, cof)  # form the linear equation system matrix
+A_, B_ = sympy.linear_eq_to_matrix(eqs, cof)  # get the linear equation system coefficient matrix
 
 solution = linalg.solve(np.mat(A_, float), np.array(B_, float))  # solve for the projective matrix
 
@@ -41,8 +42,8 @@ print("Points in xy plane by project:")
 for j in [i.reshape(3, 1) for i in [A, B, C, D]]:
     v = np.array(pro * j).reshape(3)
     v = (v / v[2])
-    print(v, end='\n')
+    print(v)
 
 print("\nOrigin points on xy plane:")
 for i in [a, b, c, d]:
-    print(i, )
+    print(i)
